1606ae97a106453422f6a61f292353439ab22be4
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2023-05-02 13:51:41 +0300

    ...

--- MVC_Joblinq/Models/City.cs
+++ MVC_Joblinq/Models/City.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace MVC_Joblinq.Models
 {
     public partial class City
     {
         public byte CityId { get; set; }
+        [DisplayName("Şehir Adı")]
         public string? CityName { get; set; }
     }
 }
